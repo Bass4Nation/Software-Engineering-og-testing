3e00ec9426e500d5aa917a69cb451be5fd9e000c
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2020-10-13 15:36:24 +0200

    nr2 commit

---
+++ oblig2/calender/src/main/java/no/kristoss/myCalender/Calender.java
@@ -0,0 +1,12 @@
+package no.kristoss.myCalender;
+
+public class Calender {
+    public static void main(String[] args) {
+        Calender prog = new Calender();
+        System.out.println(prog.message());
+    }
+
+    public String message() {
+        return "Hello!";
+    }
+}
